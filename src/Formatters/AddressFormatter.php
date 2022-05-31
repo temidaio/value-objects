7c8907b4a9c5d02de671f75719e3a8b80666e04f
Michael Rubél <contact@observer.name>
2022-05-31 09:55:52 +0200

    add address formatter

---
+++ src/Formatters/AddressFormatter.php
@@ -0,0 +1,158 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Olsza\ValueObjects\Formatters;
+
+use Illuminate\Support\Str;
+use MichaelRubel\Formatters\Formatter;
+
+class AddressFormatter implements Formatter
+{
+    /**
+     * @param string|null $prefix
+     * @param string|null $street
+     * @param string|null $number
+     * @param string|null $local
+     * @param string|null $postcode
+     * @param string|null $city
+     * @param string|null $country
+     * @param string|null $iso_code
+     */
+    public function __construct(
+        public ?string $prefix   = null,
+        public ?string $street   = null,
+        public ?string $number   = null,
+        public ?string $local    = null,
+        public ?string $postcode = null,
+        public ?string $city     = null,
+        public ?string $country  = null,
+        public ?string $iso_code = null
+    ) {
+    }
+
+    /**
+     * String builder.
+     *
+     * @var string
+     */
+    private string $builder = '';
+
+    /**
+     * Run the formatter.
+     *
+     * @return string
+     */
+    public function format(): string
+    {
+        $this->formatProperties();
+
+        return $this->buildString();
+    }
+
+    /**
+     * Initialize the internal properties.
+     *
+     * @return void
+     */
+    protected function formatProperties(): void
+    {
+        $this->street = format(
+            StreetFormatter::class,
+            prefix: $this->prefix,
+            street: $this->street,
+            number: $this->number,
+            local: $this->local,
+        );
+
+        $this->country = format(
+            CountryFormatter::class,
+            country: $this->country,
+            iso_code: $this->iso_code
+        );
+    }
+
+    /**
+     * Execute the formatter logic.
+     *
+     * @return string
+     */
+    protected function buildString(): string
+    {
+        if (! empty($this->street)) {
+            $this->appendStreet();
+
+            if (! empty($this->postcode) || ! empty($this->city) || ! empty($this->country)) {
+                $this->appendComma();
+            }
+        }
+
+        empty($this->postcode) ?: $this->appendPostcode();
+
+        empty($this->city) ?: $this->appendCity();
+
+        $this->appendCountry();
+
+        return $this->builder();
+    }
+
+    /**
+     * Sanitize the string builder.
+     *
+     * @return string
+     */
+    protected function builder(): string
+    {
+        return Str::squish($this->builder);
+    }
+
+    /**
+     * Append the street to the string builder.
+     *
+     * @return void
+     */
+    protected function appendStreet(): void
+    {
+        $this->builder .= $this->street;
+    }
+
+    /**
+     * Append the street to the string builder.
+     *
+     * @return void
+     */
+    protected function appendComma(): void
+    {
+        $this->builder .= ', ';
+    }
+
+    /**
+     * Append the post code to the string builder.
+     *
+     * @return void
+     */
+    protected function appendPostcode(): void
+    {
+        $this->builder .= $this->postcode . ' ';
+    }
+
+    /**
+     * Append the city to the string builder.
+     *
+     * @return void
+     */
+    protected function appendCity(): void
+    {
+        $this->builder .= $this->city . ' ';
+    }
+
+    /**
+     * Append the country to the string builder.
+     *
+     * @return void
+     */
+    protected function appendCountry(): void
+    {
+        $this->builder .= $this->country;
+    }
+}
---
+++ tests/Feature/Formatters/AddressFormatterTest.php
@@ -0,0 +1,121 @@
+<?php
+
+declare(strict_types=1);
+
+use Olsza\ValueObjects\Formatters\AddressFormatter;
+
+$street   = 'ul. Street 21/37';
+$country  = 'Poland';
+$postcode = '69-420';
+$city     = 'Warsaw';
+
+test('returns full address', function () use ($street, $postcode, $city, $country) {
+    $data = format(AddressFormatter::class, [
+        'street'   => $street,
+        'postcode' => $postcode,
+        'city'     => $city,
+        'country'  => $country,
+    ]);
+
+    expect($data)->toEqual('ul. Street 21/37, 69-420 Warsaw Poland');
+});
+
+test('returns full address with country iso code', function () use ($street, $postcode, $city, $country) {
+    $data = format(AddressFormatter::class, [
+        'street'   => $street,
+        'postcode' => $postcode,
+        'city'     => $city,
+        'iso_code' => 'PL',
+    ]);
+
+    expect($data)->toEqual('ul. Street 21/37, 69-420 Warsaw PL');
+});
+
+test('missing street returns partial address', function () use ($street, $postcode, $city, $country) {
+    $data = format(AddressFormatter::class, [
+        'postcode' => $postcode,
+        'city'     => $city,
+        'country'  => $country,
+    ]);
+
+    expect($data)->toEqual('69-420 Warsaw Poland');
+});
+
+test('missing postcode returining partial address', function () use ($street, $postcode, $city, $country) {
+    $data = format(AddressFormatter::class, [
+        'street'  => $street,
+        'city'    => $city,
+        'country' => $country,
+    ]);
+
+    expect($data)->toEqual('ul. Street 21/37, Warsaw Poland');
+});
+
+test('returns partial address with missing city', function () use ($street, $postcode, $city, $country) {
+    $data = format(AddressFormatter::class, [
+        'street'   => $street,
+        'postcode' => $postcode,
+        'country'  => $country,
+    ]);
+
+    expect($data)->toEqual('ul. Street 21/37, 69-420 Poland');
+});
+
+test('returns partial address with missing country', function () use ($street, $postcode, $city, $country) {
+    $data = format(AddressFormatter::class, [
+        'street'   => $street,
+        'postcode' => $postcode,
+        'city'     => $city,
+    ]);
+
+    expect($data)->toEqual('ul. Street 21/37, 69-420 Warsaw');
+});
+
+test('returns partial address with missing street & postcode', function () use ($street, $postcode, $city, $country) {
+    $data = format(AddressFormatter::class, [
+        'city'    => $city,
+        'country' => $country,
+    ]);
+
+    expect($data)->toEqual('Warsaw Poland');
+});
+
+test('returns partial address with missing postcode & city', function () use ($street, $postcode, $city, $country) {
+    $data = format(AddressFormatter::class, [
+        'street'  => $street,
+        'country' => $country,
+    ]);
+
+    expect($data)->toEqual('ul. Street 21/37, Poland');
+});
+
+test('returns partial address with missing city & country', function () use ($street, $postcode, $city, $country) {
+    $data = format(AddressFormatter::class, [
+        'street'   => $street,
+        'postcode' => $postcode,
+    ]);
+
+    expect($data)->toEqual('ul. Street 21/37, 69-420');
+});
+
+test('returns partial address with only country passed', function () use ($street, $postcode, $city, $country) {
+    $data = format(AddressFormatter::class, [
+        'country' => $country,
+    ]);
+
+    expect($data)->toEqual('Poland');
+});
+
+test('returns partial address with only street passed', function () use ($street, $postcode, $city, $country) {
+    $data = format(AddressFormatter::class, [
+        'street' => $street,
+    ]);
+
+    expect($data)->toEqual('ul. Street 21/37');
+});
+
+test('returns empty string address of all keys is missing', function () use ($street, $postcode, $city, $country) {
+    $data = format(AddressFormatter::class, []);
+
+    expect($data)->toEqual('');
+});
