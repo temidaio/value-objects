73e8392fefd48f07d3169c8808746f6a6eb769f7
Michael Rubél <contact@observer.name>
2022-05-31 09:56:05 +0200

    add country formatter

---
+++ src/Formatters/CountryFormatter.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Olsza\ValueObjects\Formatters;
+
+use Illuminate\Support\Str;
+use MichaelRubel\Formatters\Formatter;
+
+class CountryFormatter implements Formatter
+{
+    /**
+     * @param string|null $country
+     * @param string|null $iso_code
+     */
+    public function __construct(
+        public ?string $country  = null,
+        public ?string $iso_code = null
+    ) {
+    }
+
+    /**
+     * Run the formatter.
+     *
+     * @return string
+     */
+    public function format(): string
+    {
+        if (! empty($this->country)) {
+            return Str::ucfirst($this->country);
+        } elseif (! empty($this->iso_code)) {
+            return Str::upper($this->iso_code);
+        }
+
+        return '';
+    }
+}
---
+++ tests/Feature/Formatters/CountryFormatterTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+use Olsza\ValueObjects\Formatters\CountryFormatter;
+
+test('providing all data correctly returning country name', function () {
+    $data = format(CountryFormatter::class, [
+        'iso_code' => 'PL',
+        'country'  => 'pol',
+    ]);
+
+    expect($data)->toEqual('Pol');
+});
+
+test('providing only code returning code', function () {
+    $data = format(CountryFormatter::class, [
+        'iso_code' => 'PL',
+    ]);
+
+    expect($data)->toEqual('PL');
+});
+
+test('providing only name returning name', function () {
+    $data = format(CountryFormatter::class, [
+        'country' => 'poland',
+    ]);
+
+    expect($data)->toEqual('Poland');
+});
+
+test('returns empty string if wrong parameters passed', function () {
+    $data = format(CountryFormatter::class, [
+        'test' => true,
+    ]);
+
+    expect($data)->toEqual('');
+});
