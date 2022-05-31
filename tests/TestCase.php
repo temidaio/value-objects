1903bb1e4807b356d2cc6cb7f95dc01bfdb7121d
Michael Rubél <contact@observer.name>
2022-05-31 09:54:57 +0200

    update test case

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,11 +6,6 @@ use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
-    public function setUp(): void
-    {
-        parent::setUp();
-    }
-
     protected function getPackageProviders($app): array
     {
         return [];
